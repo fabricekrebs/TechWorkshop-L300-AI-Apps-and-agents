04e1e73d23012c98080b6c586d5ed1585ee96936
Fabrice Krebs <fabrice@services-web.ch>
2025-11-06 12:47:24 +0000

    Agent DevOps

--- src/app/tools/discountLogic.py
+++ src/app/tools/discountLogic.py
@@ -17,7 +17,7 @@ from azure.ai.agents.telemetry import trace_function
 import time
 from opentelemetry.instrumentation.openai_v2 import OpenAIInstrumentor
 
-# Enable Azure Monitor tracing
+#  Enable Azure Monitor tracing
 application_insights_connection_string = os.environ["APPLICATIONINSIGHTS_CONNECTION_STRING"]
 configure_azure_monitor(connection_string=application_insights_connection_string)
 OpenAIInstrumentor().instrument()
